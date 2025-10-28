070cd2d67811177f7f787ab1406e0b3ba810a6ea
Timur Subasic <subasictimur@gmail.com>
2025-10-28 18:41:24 +0100

    redirects fix

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -2,22 +2,12 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { Toaster } from "sonner";
 import Navbar from "@/components/navbar";
-import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
 
 export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = await createClient();
-
-  const { data: claims, error: claimsError } = await supabase.auth.getClaims();
-
-  if (!claims || claimsError) {
-    redirect("/");
-  }
-
   return (
     <SidebarProvider defaultOpen={false}>
       <SidebarInset>
